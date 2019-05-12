b5695963ae44b83db6ffbbee2b6c6b4bd8eb7383
ejmejm <ninjaedan@aol.com>
2019-05-12 02:44:20 -0400

    added ability to change the number of actions in the policy

--- hr_mpi_mario.py
+++ hr_mpi_mario.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
 
     ### Define starting parameters ###
     n_epochs = 1000
-    n_train_batches = 4
+    n_train_batches = 8
     n_process_batches = int(n_train_batches / n_processes)
     top_frac = 0.1
     top_x = int(np.ceil(n_processes * top_frac))
@@ -114,111 +114,113 @@ if __name__ == '__main__':
         # use_device = tf.device(f'/cpu:{rank % n_cpus}')
 
     # with use_device:
-    with tf.Session(config=device_config) as sess:
-        hasl = HASL(sess, comm, controller, rank)
+    hasl = HASL(comm, controller, rank, sess_config=device_config)
 
-        for epoch in range(1, n_epochs+1):
-            train_data = []
-            encoder_data = []
-            for _ in range(n_process_batches):
-                ### Simulate more episodes to gain training data ###
-                if rank == controller:
-                    all_data = comm.gather(worker(train_act_sets, hasl), controller)
-                    new_train_data = [x[0] for x in all_data]
-                    train_data.extend(new_train_data)
-                    encoder_data.extend([x[1] for x in all_data])
-                else:
-                    comm.gather(worker(train_act_sets, hasl), controller)
+    for epoch in range(1, n_epochs+1):
+        if epoch % 10 == 0:
+            hasl.set_act_seqs(12)
 
+        train_data = []
+        encoder_data = []
+        for _ in range(n_process_batches):
+            ### Simulate more episodes to gain training data ###
             if rank == controller:
-                encoder_data = np.concatenate(encoder_data)
-                cat_train_data = np.concatenate(train_data)
-                
-            if rank == controller:
-                print(f'----- Epoch {epoch} -----')
-
-                if epoch % log_freq == 0:
-                    log(f'Epoch {epoch} train action sets:')
-                    log(str(train_act_sets) + '\n')
-
-                ### Train reverse dynamics encoder model ###
-                assert encoder_data.shape[1] == 4
-                train_states = encoder_data[:, 0]
-                train_actions = encoder_data[:, 1]
-                train_state_ps = encoder_data[:, 3]
-
-                accuracy = hasl.train_encoder(train_states, train_state_ps, train_actions)
-                print(f'Inverse Dynamics Accuracy: {str(accuracy*100)[:5]}%')
-
-                hasl.train_policy(cat_train_data[:,0], cat_train_data[:,1], cat_train_data[:,2])
-
-                ### Pull rewards and action sequences from the training data ###
-                reward_list = []
-                # action_seq_list = []
-                for i in range(len(train_data)):
-                    reward_list.append(sum(train_data[i][:,2]))
-                    # action_seq_list.append(train_data[i][:,1])
-
-                print(f'Avg Reward: {np.mean(reward_list)}, Min: {np.min(reward_list)}, Max: {np.max(reward_list)}, Std: {np.std(reward_list)}')
-
-                ### Calculate state differences ###
-                state_changes = []
-                ss = []
-                for ep in range(len(train_data)):
-                    real_step = 0
-                    for step in range(2, len(train_data[ep])):
-                        real_step += len(train_data[ep][step][1])
-                        ss.append([real_step, train_data[ep][step][0]])
-                        state_changes.append([real_step, train_data[ep][step][0] - train_data[ep][step-2][0]])
-
-                print(f'Count: {len(state_changes)}')
-                with open('state_changes.pickle', 'wb') as f:
-                    pickle.dump(state_changes, f)
-
-                with open('ss.pickle', 'wb') as f:
-                    pickle.dump(ss, f)
-
-                ### Use softmax on rewards to stochastically choose which episodes to pull actions from ###
-                max_reward = max(reward_list)
-                scaled_rewards = [r - max_reward for r in reward_list]
-                reward_sum = sum(scaled_rewards)
-                scaled_rewards = [max(r / reward_sum, 1e-9) for r in scaled_rewards]
-
-                selected_ids = np.random.choice(range(len(train_data)), size=top_x, replace=False, p=scaled_rewards)
-                top_data = [train_data[idx] for idx in selected_ids]
-
-                ### Count all the actions of specified sizes from the chosen episodes ###
-                strain_act_sets = set([tuple(x) for x in train_act_sets])
-                branch_dicts = {}
-                for seq_len in range(min_branch, max_branch+1):
-                    count_dict = {}
-                    for episode in top_data:
-                        ep_acts = episode[:,1]
-                        for step_idx in range(seq_len-1, len(ep_acts)):
-                            new_act_set = tuple(np.concatenate(ep_acts[step_idx-seq_len+1:step_idx+1]))
-                            if tuple(new_act_set) not in strain_act_sets:
-                                if new_act_set in count_dict:
-                                    count_dict[new_act_set] += 1
-                                else:
-                                    count_dict[new_act_set] = 1
-                    
-                    branch_dicts[seq_len] = count_dict
-
-                ### Choose the new action sequences to be added to be used ###
-                top_acts = []
-                for n_branch in range(min_branch, max_branch+1):
-                    top_acts.extend([list(x[0]) for x in heapq.nlargest(act_top_x, list(branch_dicts[n_branch].items()), key=lambda x: x[1])])
-                    
-                for act in top_acts:
-                    train_act_sets.append(act)
-
-                ### Send the new action sequences to each process and sync models ###
-                comm.bcast(train_act_sets, controller)
-                hasl.sync_weights()
+                all_data = comm.gather(worker(train_act_sets, hasl), controller)
+                new_train_data = [x[0] for x in all_data]
+                train_data.extend(new_train_data)
+                encoder_data.extend([x[1] for x in all_data])
             else:
-                ### Incorporate the new action sequences into its list for further training and sync models ###
-                train_act_sets = comm.bcast(None, controller)
-                hasl.sync_weights()
+                comm.gather(worker(train_act_sets, hasl), controller)
 
-        if rank == 0:
-            print('done')
+        if rank == controller:
+            encoder_data = np.concatenate(encoder_data)
+            cat_train_data = np.concatenate(train_data)
+            
+        if rank == controller:
+            print(f'----- Epoch {epoch} -----')
+
+            if epoch % log_freq == 0:
+                log(f'Epoch {epoch} train action sets:')
+                log(str(train_act_sets) + '\n')
+
+            ### Train reverse dynamics encoder model ###
+            assert encoder_data.shape[1] == 4
+            train_states = encoder_data[:, 0]
+            train_actions = encoder_data[:, 1]
+            train_state_ps = encoder_data[:, 3]
+
+            accuracy = hasl.train_encoder(train_states, train_state_ps, train_actions)
+            print(f'Inverse Dynamics Accuracy: {str(accuracy*100)[:5]}%')
+
+            hasl.train_policy(cat_train_data[:,0], cat_train_data[:,1], cat_train_data[:,2])
+
+            ### Pull rewards and action sequences from the training data ###
+            reward_list = []
+            # action_seq_list = []
+            for i in range(len(train_data)):
+                reward_list.append(sum(train_data[i][:,2]))
+                # action_seq_list.append(train_data[i][:,1])
+
+            print(f'Avg Reward: {np.mean(reward_list)}, Min: {np.min(reward_list)}, Max: {np.max(reward_list)}, Std: {np.std(reward_list)}')
+
+            ### Calculate state differences ###
+            state_changes = []
+            ss = []
+            for ep in range(len(train_data)):
+                real_step = 0
+                for step in range(2, len(train_data[ep])):
+                    real_step += len(train_data[ep][step][1])
+                    ss.append([real_step, train_data[ep][step][0]])
+                    state_changes.append([real_step, train_data[ep][step][0] - train_data[ep][step-2][0]])
+
+            print(f'Count: {len(state_changes)}')
+            with open('state_changes.pickle', 'wb') as f:
+                pickle.dump(state_changes, f)
+
+            with open('ss.pickle', 'wb') as f:
+                pickle.dump(ss, f)
+
+            ### Use softmax on rewards to stochastically choose which episodes to pull actions from ###
+            max_reward = max(reward_list)
+            scaled_rewards = [r - max_reward for r in reward_list]
+            reward_sum = sum(scaled_rewards)
+            scaled_rewards = [max(r / reward_sum, 1e-9) for r in scaled_rewards]
+
+            selected_ids = np.random.choice(range(len(train_data)), size=top_x, replace=False, p=scaled_rewards)
+            top_data = [train_data[idx] for idx in selected_ids]
+
+            ### Count all the actions of specified sizes from the chosen episodes ###
+            strain_act_sets = set([tuple(x) for x in train_act_sets])
+            branch_dicts = {}
+            for seq_len in range(min_branch, max_branch+1):
+                count_dict = {}
+                for episode in top_data:
+                    ep_acts = episode[:,1]
+                    for step_idx in range(seq_len-1, len(ep_acts)):
+                        new_act_set = tuple(np.concatenate(ep_acts[step_idx-seq_len+1:step_idx+1]))
+                        if tuple(new_act_set) not in strain_act_sets:
+                            if new_act_set in count_dict:
+                                count_dict[new_act_set] += 1
+                            else:
+                                count_dict[new_act_set] = 1
+                
+                branch_dicts[seq_len] = count_dict
+
+            ### Choose the new action sequences to be added to be used ###
+            top_acts = []
+            for n_branch in range(min_branch, max_branch+1):
+                top_acts.extend([list(x[0]) for x in heapq.nlargest(act_top_x, list(branch_dicts[n_branch].items()), key=lambda x: x[1])])
+                
+            for act in top_acts:
+                train_act_sets.append(act)
+
+            ### Send the new action sequences to each process and sync models ###
+            comm.bcast(train_act_sets, controller)
+            hasl.sync_weights()
+        else:
+            ### Incorporate the new action sequences into its list for further training and sync models ###
+            train_act_sets = comm.bcast(None, controller)
+            hasl.sync_weights()
+
+    if rank == 0:
+        print('done')
--- model.py
+++ model.py
@@ -3,15 +3,23 @@ import tensorflow as tf
 import numpy as np
 
 class HASL():
-    def __init__(self, sess, comm, controller, rank, state_shape=(42, 42), n_actions=12):
+    def __init__(self, comm, controller, rank, sess_config=None, state_shape=(42, 42), n_base_acts=12, n_act_seqs=12):
+        if sess_config is None:
+            self.sess = tf.Session()
+        else:
+            self.sess = tf.Session(config=sess_config)
+
         self.comm = comm
         self.controller = controller
         self.rank = rank
-        self.sess = sess
-        self.create_phs(state_shape=state_shape)
+        self.sess_config = sess_config
+        self.state_shape = state_shape
+        self.n_base_acts = n_base_acts
+        self.n_act_seqs = n_act_seqs
+        self.create_phs(state_shape=self.state_shape)
         self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
-        self.create_encoder_ops(state_shape=state_shape, n_actions=n_actions)
-        self.create_policy_ops()
+        self.create_encoder_ops(state_shape=state_shape, n_base_acts=self.n_base_acts)
+        self.create_policy_ops(n_act_seqs=self.n_act_seqs)
         self.sess.run(tf.global_variables_initializer())
         self.sync_weights()
 
@@ -22,9 +30,7 @@ class HASL():
         self.act_ph = tf.placeholder(dtype=tf.int32, shape=(None,))
         self.rew_ph = tf.placeholder(dtype=tf.float32, shape=(None,))
 
-    def create_policy_ops(self):
-        self.n_acts = 12
-
+    def create_policy_ops(self, n_act_seqs=12):
         with tf.variable_scope('policy'):
             # Creating a conv net for the policy and value estimator
             self.obs_op = Input(shape=(self.enc_dim,))
@@ -33,13 +39,13 @@ class HASL():
             val_dense = Dense(128, activation='relu')(dense1)
 
             # Output probability distribution over possible actions
-            self.act_probs_op = Dense(self.n_acts, activation='softmax')(act_dense)
+            self.act_probs_op = Dense(n_act_seqs, activation='softmax', name='act_probs')(act_dense)
             self.act_out = tf.squeeze(tf.random.multinomial(tf.log(self.act_probs_op), 1))
 
             # Output value of observed state
             self.value_op = Dense(1)(val_dense)
 
-            self.act_masks = tf.one_hot(self.act_ph, self.n_acts, dtype=tf.float32)
+            self.act_masks = tf.one_hot(self.act_ph, n_act_seqs, dtype=tf.float32)
             self.log_probs = tf.log(self.act_probs_op)
 
             self.advantages = self.rew_ph - self.value_op
@@ -53,6 +59,32 @@ class HASL():
                 self.value_loss = tf.reduce_mean(tf.square(self.rew_ph - tf.squeeze(self.value_op)))
                 self.value_update = self.optimizer.minimize(self.value_loss)
 
+    def set_act_seqs(self, n_act_seqs):
+        self.n_act_seqs = n_act_seqs
+
+        ### Save weights and reset graph and session ###
+        saved_weights = self.sess.run(tf.trainable_variables())
+
+        tf.reset_default_graph()
+        self.sess.close()
+        if self.sess_config is None:
+            self.sess = tf.Session()
+        else:
+            self.sess = tf.Session(config=self.sess_config)
+
+        ### Recreate all ops ###
+        self.create_phs(state_shape=self.state_shape)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
+        self.create_encoder_ops(state_shape=self.state_shape, n_base_acts=self.n_base_acts)
+        self.create_policy_ops(n_act_seqs=self.n_act_seqs)
+
+        ### Reload applicable weights ###
+        self.sess.run(tf.global_variables_initializer())
+        t_vars = tf.trainable_variables()
+        for pair in zip(t_vars, saved_weights):
+            if 'policy/act_probs' not in pair[0].name:
+                self.sess.run(tf.assign(pair[0], pair[1]))
+
     def choose_action(self, obs, batch_size=1024):
         return self.sess.run(self.act_out, feed_dict={self.obs_op: obs})
 
@@ -65,7 +97,7 @@ class HASL():
                 self.act_ph: actions,
                 self.rew_ph: rewards})
 
-    def create_encoder_ops(self, state_shape=(42, 42), n_actions=12):
+    def create_encoder_ops(self, state_shape=(42, 42), n_base_acts=12):
         """
         Creates the encoder used for states
         """
@@ -98,10 +130,10 @@ class HASL():
         with tf.variable_scope('inverse_model'):
             self.state_state_pair = tf.concat([self.enc_state, self.enc_state_p], axis=1)
             self.im_dense = Dense(256, activation='elu')(self.state_state_pair)
-            self.act_pred = Dense(n_actions, activation='softmax', use_bias=False)(self.im_dense)
+            self.act_pred = Dense(n_base_acts, activation='softmax', use_bias=False)(self.im_dense)
 
         # State encoder train ops
-        self.act_actual_oh = tf.one_hot(self.act_ph, n_actions)
+        self.act_actual_oh = tf.one_hot(self.act_ph, n_base_acts)
         self.loss_i = tf.reduce_mean(tf.keras.losses.categorical_crossentropy(self.act_actual_oh, self.act_pred))
 
         self.im_train_vars = tf.trainable_variables(scope='encoder') + tf.trainable_variables(scope='inverse_model')
